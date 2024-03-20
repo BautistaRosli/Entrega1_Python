49f19dda7aa4c6a162deb8477353d2539b62b436
BautistaRosli <roslibautista05@gmail.com>
2024-03-20 12:13:41 -0300

    Cambio el número de intentos por un número de fallos

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ words = ["python", "programación", "computadora", "código", "desarrollo",
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 # Número máximo de intentos permitidos
-max_attempts = 10
+max_fallos = 10
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 print("¡Bienvenido al juego de adivinanzas!")
@@ -13,25 +13,29 @@ print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 word_displayed = "_" * len(secret_word)
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
-
-for i in range(max_attempts):
+i = 0
+while i < max_fallos:
     # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
     # Verificar si la letra ya ha sido adivinada
-    if letter == "" :
+    if letter == "":
         print("Ingrese una letra valida")
+        i += 1
         continue
     if letter in guessed_letters:
         print("Ya has intentado con esa letra. Intenta con otra.")
+        i += 1
         continue
     # Agregar la letra a la lista de letras adivinadas
     guessed_letters.append(letter)
  # Verificar si la letra está en la palabra secreta
-    if letter in secret_word:
+    if letter in secret_word: 
         print("¡Bien hecho! La letra está en la palabra.")
     else:
         print("Lo siento, la letra no está en la palabra.")
- # Mostrar la palabra parcialmente adivinada
+        i += 1
+
+# Mostrar la palabra parcialmente adivinada
 letters = []
 for letter in secret_word:
     if letter in guessed_letters:
@@ -42,8 +46,7 @@ word_displayed = "".join(letters)
 print(f"Palabra: {word_displayed}")
  # Verificar si se ha adivinado la palabra completa
 if word_displayed == secret_word:
-    print(f"¡Felicidades! Has adivinado la palabra secreta:
-{secret_word}")
+    print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
 else:
-    print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
+    print(f"¡Oh no! Has alcanzado los {max_fallos} fallos permitidos.")
     print(f"La palabra secreta era: {secret_word}")
